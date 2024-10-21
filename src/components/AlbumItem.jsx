abb5a960248eddb6565d0f9eca33e88e6dacdb5a
pdusarux <p.dusarux@gmail.com>
2024-10-21 12:34:48 +0700

    Create Music Album Page

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types"
+import { useNavigate } from "react-router-dom"
 
 const AlbumItem = ({ album }) => {
+    const navigate = useNavigate()
 
+    const handleClick = () => {
+        navigate(`/album/${album.id}`)
+    }
 
     return (
-        <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+        <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]" onClick={handleClick}>
             <img className="rounded" src={album.image} alt={album.name} />
             <p className="font-bold mt-2 mb-1">{album.name}</p>
             <p className="text-sm text-slate-200">{album.desc}</p>
