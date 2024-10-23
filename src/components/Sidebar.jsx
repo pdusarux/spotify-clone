4acfcfaba02249cd01b05f2a9bb0af9cbe63e678
pdusarux <p.dusarux@gmail.com>
2024-10-23 12:50:45 +0700

    Navigate to Home Page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,42 +1,52 @@
-import { assets } from '../assets/assets'
+import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const Sidebar = () => {
-    return (
-        <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
-            <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
-                <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-                    <img className='w-6' src={assets.home_icon} alt="home" />
-                    <p className='text-bold'>Home</p>
-                </div>
-                <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-                    <img className='w-6' src={assets.search_icon} alt="search" />
-                    <p className='text-bold'>Search</p>
-                </div>
-            </div>
-            <div className='bg-[#121212] h-[85%] rounded'>
-                <div className='p-4 flex items-center justify-between'>
-                    <div className='flex items-center gap-3'>
-                        <img className='w-8' src={assets.stack_icon} alt="library" />
-                        <p className='text-semibold'>Your Library</p>
-                    </div>
-                    <div className='flex items-center gap-3'>
-                        <img className='w-5' src={assets.arrow_icon} alt="arrow" />
-                        <img className='w-5' src={assets.plus_icon} alt="plus" />
-                    </div>
-                </div>
-                <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
-                    <h1>Create your first playlist</h1>
-                    <p className='font-light'>It&apos;s easy, we will help you</p>
-                    <button className='bg-white text-[15px] text-black px-4 py-1.5 rounded-full mt-4'>Create playlist</button>
-                </div>
-                <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
-                    <h1>Let&apos;s find some podcasts to follow</h1>
-                    <p className='font-light'>We will keep you updated on new episodes</p>
-                    <button className='bg-white text-[15px] text-black px-4 py-1.5 rounded-full mt-4'>Browse podcasts</button>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const navigate = useNavigate();
 
-export default Sidebar
+  return (
+    <div
+      onClick={() => navigate("/")}
+      className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex"
+    >
+      <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
+        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+          <img className="w-6" src={assets.home_icon} alt="home" />
+          <p className="text-bold">Home</p>
+        </div>
+        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+          <img className="w-6" src={assets.search_icon} alt="search" />
+          <p className="text-bold">Search</p>
+        </div>
+      </div>
+      <div className="bg-[#121212] h-[85%] rounded">
+        <div className="p-4 flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <img className="w-8" src={assets.stack_icon} alt="library" />
+            <p className="text-semibold">Your Library</p>
+          </div>
+          <div className="flex items-center gap-3">
+            <img className="w-5" src={assets.arrow_icon} alt="arrow" />
+            <img className="w-5" src={assets.plus_icon} alt="plus" />
+          </div>
+        </div>
+        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
+          <h1>Create your first playlist</h1>
+          <p className="font-light">It&apos;s easy, we will help you</p>
+          <button className="bg-white text-[15px] text-black px-4 py-1.5 rounded-full mt-4">
+            Create playlist
+          </button>
+        </div>
+        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4">
+          <h1>Let&apos;s find some podcasts to follow</h1>
+          <p className="font-light">We will keep you updated on new episodes</p>
+          <button className="bg-white text-[15px] text-black px-4 py-1.5 rounded-full mt-4">
+            Browse podcasts
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
